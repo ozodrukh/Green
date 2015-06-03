619f93bdfd1b7b1a0aa528e6947b756f6910e8d5
Ozodrukh <ozik.dev@gmail.com>
2015-06-03 13:06:34 +0500

    fixes

--- sample/src/main/java/codetail/green/sample/MainActivity.java
+++ sample/src/main/java/codetail/green/sample/MainActivity.java
@@ -1,9 +1,7 @@
 package codetail.green.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import codetail.widget.green.ConfigurationWidget;
 
@@ -15,28 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ConfigurationWidget widget = (ConfigurationWidget) findViewById(R.id.ConfigWidget);
-        widget.setConfigurations(R.raw.config_file_sample_base);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(savedInstanceState == null) {
+            widget.setConfigurations(R.raw.config_file_sample_base);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
