5c611ac1701bb72b5c43444368fb05222e5ec156
Ozodrukh <ozik.dev@gmail.com>
2015-06-04 18:15:40 +0500

    missed files added

--- sample/src/developer/java/codetail/green/sample/DeveloperActivity.java
+++ sample/src/developer/java/codetail/green/sample/DeveloperActivity.java
@@ -9,7 +9,6 @@ public class DeveloperActivity extends MainActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         ConfigurationWidget widget = (ConfigurationWidget) findViewById(R.id.ConfigWidget);
         widget.setConfigurations(R.raw.config_file_sample_base, savedInstanceState != null);
